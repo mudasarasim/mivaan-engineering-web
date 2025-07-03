b2902a44b2d994c9eff0bcbab818c059a519d4ad
Vishwesh Patil <vishveshpatil99@gmail.com>
2025-07-04 00:12:16 +0400

    Added ISO p tag.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,10 +45,10 @@ const Footer = () => {
         <Row>
           <Col>
             <p className="mb-1 text-muted">
-              Copyright 2025 © ACC. All rights reserved.
+              Copyright 2025 © Mivaan Group. All rights reserved.
             </p>
             <p className="text-muted small">
-              ACC Group Ltd and its affiliated entities make every effort to maintain the information stored on this website as current as possible. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+              ISO 9001:2015,ISO14001:2015 & ISO 45001: 2018 Certified company for Mivaan Engineering Consultancy & Mivaan General Contracting
             </p>
           </Col>
         </Row>
