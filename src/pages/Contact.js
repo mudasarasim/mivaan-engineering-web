83516ed116d032cd97207d46ad3ff36f016aad79
Vishwesh Patil <vishveshpatil99@gmail.com>
2025-07-07 10:07:10 +0400

    Updated Location.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -57,32 +57,32 @@ const ContactUs = () => {
       {/* Contact and Map Section */}
       <section className="contact-section py-5">
         <div className="container">
-          <div className="row text-center mb-5">
+          <div className="row justify-content-center  text-center mb-5">
             <div className="col-md-4">
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-envelope fa-2x"></i></div>
                 <h5>Mail</h5>
-                <p><i className="fa fa-envelope"></i> info@mivaan.com</p>
+                <p><i className="fa fa-envelope"></i>info@mivaanengineering.ae</p>
               </div>
             </div>
-            <div className="col-md-4">
+            {/* <div className="col-md-4">
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-phone fa-2x"></i></div>
                 <h5>Contact</h5>
                 <p><i className="fa fa-mobile"></i> +971 52 635 3298</p>
               </div>
-            </div>
+            </div> */}
             <div className="col-md-4">
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-map-marker fa-2x"></i></div>
                 <h5>Address</h5>
-                <p>Dubai - United Arab Emirates</p>
+                <p>Dar Al Salam Building, Abu Dhabi</p>
               </div>
             </div>
           </div>
 
           <div className="row align-items-stretch">
-            <div className="col-lg-6 mb-4">
+            {/* <div className="col-lg-6 mb-4">
               <form onSubmit={handleSubmit} className="p-4 shadow-lg rounded-4 h-100">
                 <div className="form-group mb-3">
                   <input
@@ -129,19 +129,21 @@ const ContactUs = () => {
                 </div>
                 <button type="submit" className="btn btn-dark w-100">Submit</button>
               </form>
-            </div>
+            </div> */}
 
-            <div className="col-lg-6 mb-4">
-              <div className="h-100 rounded-4 overflow-hidden shadow-lg">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3630.8216853370423!2d54.35481007481491!3d24.491634459917925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e67116a7e2e9f%3A0x401cc698d98d4dd5!2sDar%20al%20Salam%20Building!5e0!3m2!1sen!2sae!4v1751533452885!5m2!1sen!2sae"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  style={{ border: 0, minHeight: '100%' }}
-                  allowFullScreen
-                  title="Google Map"
-                ></iframe>
+            <div className="row">
+              <div className="col-12 px-0">
+                <div className="w-100" style={{ height: '500px' }}>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3630.8216853370423!2d54.35481007481491!3d24.           491634459917925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.          1!3m3!1m2!1s0x3e5e67116a7e2e9f%3A0x401cc698d98d4dd5!2sDar%20al%20Salam%20Building!5e0!3m2!1sen!2sae!4v1751533452885!5m2!1sen!2sae "
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    title="Google Map"
+                  ></iframe>
+                </div>
               </div>
             </div>
           </div>
